59d7a553d6028cacd776b3d2f17436c770ecddb7
Ray Strode <rstrode@redhat.com>
2024-12-12 08:47:43 -0500

    build: Add "Build Extension" task
    Now that we have the basics of the project tree sketched out, we
    need a way to build it.
    
    This commit adds:
    
    - a new "Build Extension" task for VS Code to use, which calls
    - a "build" script definition in package.json to fire the build off
      from node, which calls
    - a build.js script to do the actual building
    
    Significant contribution by Owen Taylor to:
    
    - dynamically rewrite strings in the source to aid in the Granite.Code
      branding effort (as a better alternative to an older work in progress
      approach of transforming the source on disk using ts-morph)
    - change the build output directory to more closely match upstream continue

---
+++ scripts/build.js
@@ -0,0 +1,104 @@
+const esbuild = require("esbuild");
+const fs = require("node:fs");
+const path = require("node:path");
+
+const LOG_LEVEL = "info";
+const MODULE_BUNDLE_BLOCK_LIST = [
+  "esbuild",
+  "vscode",
+  "./xhr-sync-worker.js",
+  "onnxruntime-node",
+  "win-ca",
+];
+
+// esbuild plugin to Transform VSCode string identifiers like
+// configuration or command IDs from the `continue.` namespace
+// to the `granite.` namespace to avoid clashes
+
+// Quick-and-not-quite-right replacement for not-yet-common RegExp.escape()
+function escapeForRegExp(str) {
+  return str.replace(/[|\\*?+$^.{}()\[\]]/g, "\\$&");
+}
+
+const extensionPath = path.resolve(
+  __dirname,
+  "../continue/extensions/vscode/src",
+);
+const extensionFilter = new RegExp(
+  "^" + escapeForRegExp(extensionPath) + "/.*\\.ts$",
+);
+
+const namespaceSubstitutions = [
+  {
+    pattern: /^continue[.]/,
+    replacement: "granite.",
+  },
+  {
+    pattern: /^Continue[.]continue$/,
+    replacement: "redhat.granite-code",
+  },
+  {
+    pattern: /continueGUI/,
+    replacement: "graniteGUI",
+  },
+];
+
+function namespaceTransform(fileContents) {
+  // For speed, we do this just as a string replacement, rather than
+  // using ts-morph or similar. We only care about simple strings
+  // like "continue.acceptDiff", so we can simplify matching
+  // strings in the source code.
+  return fileContents.replace(/"([^"\\\r\n]+)"/g, (m, strContents) => {
+    for (const { pattern, replacement } of namespaceSubstitutions) {
+      strContents = strContents.replace(pattern, replacement);
+    }
+    return '"' + strContents + '"';
+  });
+}
+
+const namespaceTransformPlugin = {
+  name: "granite-namespace-transform",
+  setup(build) {
+    build.onLoad(
+      {
+        filter: extensionFilter,
+      },
+      async (args) => {
+        const text = await fs.promises.readFile(args.path, "utf8");
+        return {
+          contents: namespaceTransform(text),
+          loader: "ts",
+        };
+      },
+    );
+  },
+};
+
+const buildConfig = {
+  entryPoints: ["./extension.ts"],
+  bundle: true,
+  external: MODULE_BUNDLE_BLOCK_LIST,
+  format: "cjs",
+  logLevel: LOG_LEVEL,
+  minify: false,
+  sourcemap: true,
+  sourcesContent: true,
+  outfile: "./out/index.js",
+  plugins: [namespaceTransformPlugin],
+  platform: "node",
+
+  // Workaround, see: https://github.com/evanw/esbuild/issues/1492#issuecomment-893144483
+  inject: ["continue/extensions/vscode/scripts/importMetaUrl.js"],
+  define: { "import.meta.url": "importMetaUrl" },
+};
+
+async function main() {
+  const buildContext = await esbuild.context(buildConfig);
+  await buildContext.rebuild();
+  await buildContext.dispose();
+}
+
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
