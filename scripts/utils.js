ed95e8d4662bf4a1da71367366d663f897caafc5
Owen W. Taylor <otaylor@fishsoup.net>
2025-01-29 00:55:12 -0500

    build: Add a runCommand() utility function
    When scripts are run in VSCode the output is comingled into
    one pane.
    
    For clarity, it's useful to know if the output is coming directly
    from our scripts or is, e.g., coming from npm.
    
    This commit adds a utility function, runCommand, to run commands
    with each line annotated with a designated prefix.
    
    Contribution by Ray Strode to avoid needing a split2 dependency.

---
+++ scripts/utils.js
@@ -0,0 +1,68 @@
+const { spawn } = require("child_process");
+const { Transform } = require("stream");
+
+function waitForSubprocess(subprocess, command) {
+  return new Promise((resolve, reject) => {
+    subprocess.on("close", (code) => {
+      if (code == 0) {
+        resolve(true);
+      } else {
+        reject(new Error(`'${command.join(" ")}' exited with status ${code}`));
+      }
+    });
+
+    subprocess.on("error", (err) => {
+      reject(new Error(`'${command.join(" ")}' failed: ${err.message}`));
+    });
+  });
+}
+
+class LineStream extends Transform {
+  constructor() {
+    super({ readableObjectMode: true });
+    this.partialLine = "";
+  }
+
+  _transform(chunk, encoding, callback) {
+    this.partialLine += chunk.toString();
+    const lines = this.partialLine.split(/\r?\n/);
+    this.partialLine = lines.pop();
+
+    lines.forEach((line) => this.push(line));
+    callback();
+  }
+
+  _flush(callback) {
+    if (this.partialLine) this.push(this.partialLine);
+
+    callback();
+  }
+}
+
+/**
+ * Convenience async function to run a command, prefixing the
+ * each line of stderr/stdout with a given string.
+ *
+ * @param {string} outputPrefix - prefix to add to each line
+ * @param {string|Array} command - Command to run
+ * @param {string|undefined} cwd - working directory
+ */
+async function runCommand(outputPrefix, command, cwd) {
+  if (typeof command == "string") command = command.split(/\s+/);
+
+  const options = {};
+  if (cwd) {
+    options.cwd = cwd;
+  }
+  const subprocess = spawn(command[0], command.slice(1), options);
+  subprocess.stdout.pipe(new LineStream()).on("data", (line) => {
+    console.error("%s%s", outputPrefix, line);
+  });
+  subprocess.stderr.pipe(new LineStream()).on("data", (line) => {
+    console.error("%s%s", outputPrefix, line);
+  });
+
+  await waitForSubprocess(subprocess, command);
+}
+
+exports.runCommand = runCommand;
