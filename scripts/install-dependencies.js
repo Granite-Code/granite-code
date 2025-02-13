957f85f241321193d9dacae5e03a8c864656f8a1
Ray Strode <rstrode@redhat.com>
2025-02-13 15:13:45 -0500

    build: Provide way to stage arch specific node_modules
    In order to generate a VSIX package, we need to have node
    modules for platforms that may differ from the build host.
    
    This commit augments the install-dependencies script to allow
    it to pull down node modules for other platforms to a staging
    area, where they can later be plucked up to use during VSIX
    packaging.

--- scripts/install-dependencies.js
+++ scripts/install-dependencies.js
@@ -3,9 +3,10 @@ const { promisify } = require("util");
 const fs = require("fs").promises;
 const { runCommand } = require("./utils.js");
 
-// The glob module may not be available until after nodeModulesInstallation returns
-// so it'll get imported later
+// The glob and minimatch modules may not be available until after nodeModulesInstallation returns
+// so they'll get imported later
 let glob;
+let minimatch;
 
 const dependencyConfiguration = {
   entries: [
@@ -51,7 +52,7 @@ const installConfiguration = {
     {
       description: "SQLite library",
       inputFiles: ["continue/core/node_modules/sqlite3/build/Release/*.node"],
-      outputDir: "Release/",
+      outputDir: "build/Release/",
     },
 
     {
@@ -59,6 +60,45 @@ const installConfiguration = {
       inputFiles: [
         "continue/extensions/vscode/node_modules/jsdom/lib/jsdom/living/xhr/xhr-sync-worker.js",
       ],
+    },
+
+    {
+      description: "LanceDB vector database for macOS (arm64)",
+      inputModules: ["@lancedb/vectordb-darwin-arm64"],
+      platforms: ["darwin-arm64"],
+      outputDir: "../node_modules:darwin-arm64",
+    },
+
+    {
+      description: "LanceDB vector database for Linux (arm64)",
+      inputModules: ["@lancedb/vectordb-linux-arm64-gnu"],
+      platforms: ["linux-arm64"],
+      outputDir: "../node_modules:linux-arm64",
+    },
+
+    {
+      description: "LanceDB vector database for Linux (x86-64)",
+      inputModules: ["@lancedb/vectordb-linux-x64-gnu"],
+      platforms: ["linux-x64"],
+      outputDir: "../node_modules:linux-x64",
+    },
+    {
+      description: "esbuild binaries for macOS (arm64)",
+      inputModules: ["esbuild-darwin-arm64"],
+      outputDir: "../node_modules:darwin-arm64",
+      platforms: ["darwin-arm64"],
+    },
+    {
+      description: "esbuild binaries for Linux (arm64)",
+      inputModules: ["esbuild-linux-arm64"],
+      outputDir: "../node_modules:linux-arm64",
+      platforms: ["linux-arm64"],
+    },
+    {
+      description: "esbuild binaries for Linux (x86-64)",
+      inputModules: ["esbuild-linux-64"],
+      outputDir: "../node_modules:linux-x64",
+      platforms: ["linux-x64"],
     },
   ],
   root: "./out",
@@ -95,6 +135,8 @@ async function packageDependenciesInstallation() {
 async function runtimeAssetsCopy() {
   let willCopyFiles = false;
   for (const entry of installConfiguration.entries) {
+    if (!entry.inputFiles) continue;
+
     let willCopyFilesForEntry = false;
     const inputFiles = [];
     for (const filePattern of entry.inputFiles) {
@@ -152,13 +194,71 @@ async function baseDependenciesInstallation() {
   console.log("\nInstalling base dependencies…");
   await nodeModulesInstallation(".");
 
-  // glob should be available now
+  // glob and minimatch should be available now
   glob = require("glob").glob;
+  minimatch = require("minimatch").minimatch;
+}
+
+async function platformDependenciesInstallation(target) {
+  let installed = false;
+
+  for (const entry of installConfiguration.entries) {
+    if (!entry.inputModules) continue;
+
+    let platform, architecture;
+    if (target) {
+      if (!entry.platforms?.some((pattern) => minimatch(target, pattern))) {
+        continue;
+      }
+      [platform, architecture] = target.split("-");
+    } else if (entry.platforms) {
+      [platform, architecture] = entry.platforms[0].split("-");
+    } else {
+      platform = process.platform;
+      architecture = process.arch;
+    }
+
+    const outputDir = path.resolve(
+      installConfiguration.root,
+      entry.outputDir ?? "./",
+    );
+
+    if (!installed) {
+      console.log("\nInstalling platform-specific dependencies...");
+      installed = true;
+    }
+
+    console.log(`\nInstalling ${entry.description}…`);
+
+    await fs.mkdir(outputDir, { recursive: true });
+    await runCommand("npm:     ", [
+      "npm",
+      "install",
+      ...entry.inputModules,
+      "--architecture=" + architecture,
+      "--platform=" + platform,
+      "--force",
+      "--prefix",
+      outputDir,
+    ]);
+  }
+
+  if (installed) {
+    console.log("\nPlatform-specific dependencies installed.");
+  }
 }
 
 async function main() {
+  const args = process.argv.slice(2);
+  let target;
+
+  if (args[0] === "--target") {
+    target = args[1];
+  }
+
   await baseDependenciesInstallation();
   await packageDependenciesInstallation();
+  await platformDependenciesInstallation(target);
   await runtimeAssetsCopy();
 }
 
