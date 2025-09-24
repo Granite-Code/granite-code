Granite-Code/granite-code

commit b17613ccee72d768dc9836d94541a6794b0222ed

Add start of extension.ts
We need a entry point for the extension to start things off.

This commit adds one based on the continue extension, but
without telemetry.

extension.ts
@@ -1,0 +1,36 @@
+/**
+ * This is the entry point for the extension.
+ */
+import * as vscode from "vscode";
+import { setupCa } from "./continue/core/util/ca";
+
+async function activateAsync(context: vscode.ExtensionContext) {
+  await setupCa();
+  const { activateExtension } = await import(
+    "./continue/extensions/vscode/src/activation/activate"
+  );
+  console.log("activating extension");
+  return await activateExtension(context);
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  return activateAsync(context).catch((e) => {
+    console.log("Error activating extension: ", e);
+    vscode.window
+      .showWarningMessage(
+        "Error activating the Granite.Code extension.",
+        "View Logs",
+        "Retry",
+      )
+      .then((selection) => {
+        if (selection === "View Logs") {
+          vscode.commands.executeCommand("granite.viewLogs");
+        } else if (selection === "Retry") {
+          // Reload VS Code window
+          vcode.commands.executeCommand("workbench.action.reloadWindow");
+        }
+      });
+  });
+}
+
+export function deactivate() {}

commit c739ddd57324715b20b4803900df7b4295ae6ea7

extension.ts: Fix typo
Otherwise, it leads to:
  Cannot find name 'vcode'. Did you mean 'vscode'? ts(2552) [Ln 30,
      Col 11]

Fallout from b17613ccee72d768dc9836d94541a6794b0222ed

https://github.com/Granite-Code/granite-code/pull/158

extension.ts
@@ -27,7 +27,7 @@ export function activate(context: vscode.ExtensionContext) {
           vscode.commands.executeCommand("granite.viewLogs");
         } else if (selection === "Retry") {
           // Reload VS Code window
-          vcode.commands.executeCommand("workbench.action.reloadWindow");
+          vscode.commands.executeCommand("workbench.action.reloadWindow");
         }
       });
   });